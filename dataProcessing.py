4c5b23445f7eb72e3710a78148c2f12621e1c978
huang-me <amos0107@gmail.com>
2021-06-12 21:09:48 +0800

    Make full preprocess into a function

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,10 +2,13 @@ from src import get_train, load_train
 
 import pandas as pd
 
-if __name__ == "__main__":
+def fullPreprocess():
     # load data from files
     data = load_train()
     # preprocess datas
     df = get_train(data)
     # output to file
     df.to_csv('processed.csv', index=False)
+
+if __name__ == "__main__":
+    fullPreprocess()
